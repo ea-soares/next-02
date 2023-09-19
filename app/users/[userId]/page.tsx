6e605cb967a5631082fe3def6e491024b83b6363
Alexandre Soares <easoares@gmail.com>
2023-09-18 22:29:25 -0300

    lesson 2 - 1h03

---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,13 @@
+type Params = {
+    params: {
+        userId: string
+    }
+}
+
+export default function UserPage({ params: {userId}}: Params) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
